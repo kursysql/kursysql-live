26e9a38423aa1797c22d3d62f6a1e30725be8604
Tomasz Libera <tomasz.libera@outlook.com>
2021-01-10 19:07:05 +0100

    Northwind new folder

---
+++ webinary/20200111 PodstawyZapytan/Northwind.sql
@@ -0,0 +1,14 @@
+/*
+	Northwind
+	Podstawy zapytań
+
+	libera@kursysql.pl
+	https://github.com/microsoft/sql-server-samples/blob/master/samples/databases/northwind-pubs/instnwnd.sql
+*/
+
+
+USE Northwind
+GO
+
+
+
